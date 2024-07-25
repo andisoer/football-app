ff5a11eac757990a377fae22b3dac7ec4c0ec808
Andi Surya <andi502.as@gmail.com>
2024-07-25 11:39:50 +0700

    Initial Commit

---
+++ movieapp/Models/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  movieapp
+//
+//  Created by Andi Surya on 7/25/24.
+//
+
+import Foundation
+
---
+++ movieapp/movieappApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieappApp.swift
+//  movieapp
+//
+//  Created by Andi Surya on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct movieappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
